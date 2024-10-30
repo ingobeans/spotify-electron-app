523cf7e1609cfc22920a84f200abe07b08e6bf7b
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-10-30 21:35:55 +0100

    added plugins

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const { app, components, BrowserWindow, ipcMain } = require("electron");
-
 const path = require("path");
+const fs = require("fs");
+
+let pluginsDir = path.join(__dirname, "plugins");
+
+function loadPlugins() {
+  let arr = [];
+  fs.readdir(pluginsDir, (err, files) => {
+    if (err) {
+      console.error("error reading dir: ", err);
+      return;
+    }
+
+    files.forEach((file) => {
+      if (path.extname(file) === ".js") {
+        const filePath = path.join(pluginsDir, file);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+
+        arr.push(fileContent);
+      }
+    });
+  });
+  return arr;
+}
+
+let plugins = loadPlugins();
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -18,6 +42,7 @@ function createWindow() {
 
   const userAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.62 Safari/537.36";
+  // weirdly enough this is required lest the regular web player isn't shown
   win.webContents.setUserAgent(userAgent);
 
   win.loadURL("https://open.spotify.com/");
@@ -27,6 +52,17 @@ function createWindow() {
     if (navigationHistory.canGoBack()) {
       navigationHistory.goBack();
     }
+  });
+
+  win.webContents.on("did-frame-finish-load", () => {
+    win.webContents.executeJavaScript(`
+      console.log('hellooo!');
+    `);
+
+    // run plugins
+    plugins.forEach((code) => {
+      win.webContents.executeJavaScript(code);
+    });
   });
 }
 
--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 for some reason the spotify app sometimes doesn't work at all for me. the browser version does though, so i just made an electron app that just displays the web version. so in practice it's almost indistinguishable from the real app, just being more standard and hackable.
 
 cool!
+
+# plugins?
+
+yeah i also added plugin support (why not). all scripts in plugins/ dir will be ran as soon as the page loads.
+go ahead and customise spotify to your heart's content!
+by default there's only a single script there that just removes the "Install App" button.
